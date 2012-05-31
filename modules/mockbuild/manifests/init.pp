c3ffc9166a7eb0184a3af47991e43f1749613627
Chris AtLee <catlee@mozilla.com>
2012-05-30 22:34:00 -0400

    mockbuild module

---
+++ modules/mockbuild/manifests/init.pp
@@ -0,0 +1,21 @@
+class mockbuild {
+    include packages::mozilla::mock_mozilla
+    file {
+        "/etc/mock_mozilla/mozilla-f11-i386.cfg":
+            require => Class["packages::mozilla::mock_mozilla"],
+            ensure => present,
+            source => "puppet:///modules/mockbuild/mozilla-f11-i386.cfg";
+        "/etc/mock_mozilla/mozilla-f16-i386.cfg":
+            require => Class["packages::mozilla::mock_mozilla"],
+            ensure => present,
+            source => "puppet:///modules/mockbuild/mozilla-f16-i386.cfg";
+        "/etc/mock_mozilla/mozilla-centos6-i386.cfg":
+            require => Class["packages::mozilla::mock_mozilla"],
+            ensure => present,
+            source => "puppet:///modules/mockbuild/mozilla-centos6-i386.cfg";
+        "/etc/mock_mozilla/mozilla-centos6-x86_64.cfg":
+            require => Class["packages::mozilla::mock_mozilla"],
+            ensure => present,
+            source => "puppet:///modules/mockbuild/mozilla-centos6-x86_64.cfg";
+    }
+}
