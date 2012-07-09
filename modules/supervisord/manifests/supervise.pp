ef8985bafed181d196d0f5a79c2fec6d5e81a7b1
Chris AtLee <catlee@mozilla.com>
2012-07-09 14:43:46 -0400

    integrate service template

--- modules/supervisord/manifests/supervise.pp
+++ modules/supervisord/manifests/supervise.pp
@@ -6,7 +6,7 @@ define supervisord::supervise($command, $user, $autostart=true, $autorestart=tru
             content => template("supervisord/snippet.erb"),
             notify => Exec["supervisord_make_config"];
         "/etc/init.d/supervisord-$name":
-            content => "#!/bin/sh",
+            content => template("supervisord/service.erb"),
             mode => 0755;
     }
 
@@ -14,10 +14,6 @@ define supervisord::supervise($command, $user, $autostart=true, $autorestart=tru
         "supervisord-$name":
             require => File["/etc/init.d/supervisord-$name"],
             enable => true,
-            ensure => running,
-            status => "/usr/bin/supervisorctl status $name",
-            restart => "/usr/bin/supervisorctl restart $name",
-            start => "/usr/bin/supervisorctl start $name",
-            stop => "/usr/bin/supervisorctl stop $name";
+            ensure => running;
     }
 }
